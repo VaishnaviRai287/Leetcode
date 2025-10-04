1701de0f19764ee9fe60cab4148f7e3b8f08d9b9
VaishnaviRai287 <144893427+VaishnaviRai287@users.noreply.github.com>
2025-10-05 04:16:51 +0530

    Time: 83 ms (9.49%) | Memory: 94.1 MB (19.47%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int k){
-        if(k<0) return 0;
-        int l=0, r=0, sum=0, count=0;
-        while( r < nums.size()){
-            sum += nums[r]%2;
-            while(sum>k){
-                sum -= nums[l]%2;
-                l++;
-            }
-            count += r-l+1;
-            r++;
-        }
-        return count;
-    }
+    
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return helper(nums,k) - helper(nums, k-1);
+        int currSum = 0, subarrays = 0;
+        unordered_map<int, int> prefixSum;
+        prefixSum[currSum] = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            currSum += nums[i] % 2;
+            if (prefixSum.find(currSum - k) != prefixSum.end()) {
+                subarrays = subarrays + prefixSum[currSum - k];
+            }
+            prefixSum[currSum]++;
+        }
+
+        return subarrays;
     }
 };
