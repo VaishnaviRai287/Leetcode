e35f1c3bc34f12bbd72e6862cc28d8a47f117549
VaishnaviRai287 <144893427+VaishnaviRai287@users.noreply.github.com>
2026-02-10 23:17:47 +0530

    Time: 0 ms (100.00%) | Memory: 128 MB (98.44%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -10,60 +10,32 @@
  * };
  */
 class Solution {
+    int maxDistance = 0;
+
+    int dfs(TreeNode* root, int start) {
+        if (!root) return 0;
+
+        int left = dfs(root->left, start);
+        int right = dfs(root->right, start);
+        if (root->val == start) {
+            maxDistance = max(maxDistance, max(left, right));
+            return -1;
+        }
+        if (left < 0) {
+            int dist = abs(left);
+            maxDistance = max(maxDistance, dist + right);
+            return left - 1;
+        }
+        if (right < 0) {
+            int dist = abs(right);
+            maxDistance = max(maxDistance, dist + left);
+            return right - 1;
+        }
+        return max(left, right) + 1;
+    }
 public:
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<TreeNode*, TreeNode*> parent;
-        TreeNode* startNode = nullptr;
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* cur = q.front();
-            q.pop();
-
-            if (cur->val == start) startNode = cur;
-
-            if (cur->left) {
-                parent[cur->left] = cur;
-                q.push(cur->left);
-            }
-            if (cur->right) {
-                parent[cur->right] = cur;
-                q.push(cur->right);
-            }
-        }
-        unordered_set<TreeNode*> visited;
-        queue<TreeNode*> bfs;
-        bfs.push(startNode);
-        visited.insert(startNode);
-
-        int time = -1;
-
-        while (!bfs.empty()) {
-            int size = bfs.size();
-            time++;
-
-            while (size--) {
-                TreeNode* node = bfs.front();
-                bfs.pop();
-
-                if (node->left && !visited.count(node->left)) {
-                    visited.insert(node->left);
-                    bfs.push(node->left);
-                }
-
-                if (node->right && !visited.count(node->right)) {
-                    visited.insert(node->right);
-                    bfs.push(node->right);
-                }
-
-                if (parent.count(node) && !visited.count(parent[node])) {
-                    visited.insert(parent[node]);
-                    bfs.push(parent[node]);
-                }
-            }
-        }
-
-        return time;
+        dfs(root, start);
+        return maxDistance;
     }
 };
