d0bd3ce0cdecc934690e90d984bb6a9568f5290a
VaishnaviRai287 <144893427+VaishnaviRai287@users.noreply.github.com>
2026-03-29 23:04:27 +0530

    Time: 21 ms (31.51%) | Memory: 36 MB (65.28%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        sort(arr.begin(), arr.end(), [&](int a, int b){
+            if(abs(a-x) == abs(b-x)) return a<b;
+            return abs(a-x) < abs(b-x);
+        });
+        vector<int> res(arr.begin(), arr.begin()+k);
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
