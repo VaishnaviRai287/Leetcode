6cc0a40d75b156707a29cfe8dd42dd35d31b7140
VaishnaviRai287 <144893427+VaishnaviRai287@users.noreply.github.com>
2026-06-11 17:24:49 +0530

    Time: 1 ms (97.12%) | Memory: 34 MB (51.75%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int m,n;
+    void dfs(vector<vector<int>>& grid, int r, int c){
+        if(r<0 || c<0 || r>=m || c>=n || grid[r][c] != 1){
+            return;
+        }
+        grid[r][c] = -1;
+        dfs(grid, r+1, c);
+        dfs(grid, r-1, c);
+        dfs(grid, r, c+1);
+        dfs(grid, r, c-1);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+
+        for(int i =0 ; i<m; i++){
+            if(grid[i][0] == 1) dfs(grid, i, 0);
+            if(grid[i][n-1] == 1) dfs(grid, i, n-1);
+        }
+        for(int j = 0; j<n; j++){
+            if(grid[0][j]==1) dfs(grid, 0, j);
+            if(grid[m-1][j] == 1) dfs(grid, m-1, j);
+        }
+        int count = 0;
+        for(int i = 0; i<m; i++){
+            for(int j =0; j<n; j++){
+                if(grid[i][j] == 1) count++;
+            }
+        }
+        return count;
+    }
+};
