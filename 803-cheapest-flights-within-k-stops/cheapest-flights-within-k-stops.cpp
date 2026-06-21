139f00d8f6fe9406c31f0c1feef678558f120e99
Vaishnavi Rai <144893427+VaishnaviRai287@users.noreply.github.com>
2026-06-21 23:50:37 +0530

    Time: 7 ms (27.17%) | Memory: 17.2 MB (91.57%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int, int>> adj[n];
-        for (auto it : flights) {
-            adj[it[0]].push_back({it[1], it[2]});
-        }
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0, {src, 0}});
         vector<int> dist(n, 1e9);
         dist[src] = 0;
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int stops = it.first;
-            int node = it.second.first;
-            int cost = it.second.second;
-            if (stops > k)
-                continue;
 
-            for (auto iter : adj[node]) {
+        for (int i = 0; i <= k; i++) {
 
-                int adjNode = iter.first;
-                int edW = iter.second;
+            vector<int> temp = dist;
 
-                if (cost + edW < dist[adjNode] && stops <= k) {
+            for (auto &flight : flights) {
 
-                    dist[adjNode] = cost + edW;
-                    q.push({
-                        stops + 1,
-                        {adjNode, cost + edW}
-                    });
-                }
+                int u = flight[0];
+                int v = flight[1];
+                int wt = flight[2];
+
+                if (dist[u] == 1e9)
+                    continue;
+
+                temp[v] = min(temp[v], dist[u] + wt);
             }
-        }
-        if (dist[dst] == 1e9)
-            return -1;
 
-        return dist[dst];
+            dist = temp;
+        }
+
+        return dist[dst] == 1e9 ? -1 : dist[dst];
 
     }
 };
