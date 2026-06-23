b3b5af2b58c18403b3a3eb8e9a029ffab454895b
Vaishnavi Rai <144893427+VaishnaviRai287@users.noreply.github.com>
2026-06-24 03:32:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (62.21%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,23 +1,53 @@
-class Solution {
+class DisjointSet{
+vector<int> size, parent;
 public:
-    void dfs(int start, vector<vector<int>>& isConnected, vector<int>& vis){
-        vis[start] = 1;
-        for(int j = 0; j < isConnected.size(); j++) {
-            if(isConnected[start][j] == 1 && !vis[j]) {
-                dfs(j, isConnected, vis);
-            }
+    DisjointSet(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i = 0; i<=n ; i++){
+            parent[i] = i;
         }
     }
+    int findUpar(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUpar(parent[node]);
+    }
+    void unionbysize(int u, int v){
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_v]<size[ulp_u]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V = isConnected.size();
-        vector<int> vis(V, 0);
-        int count = 0;
-        for(int i = 0; i<V; i++){
-            if(!vis[i]){
-                count++;
-                dfs(i, isConnected, vis);
+        int m = isConnected.size();
+        int n = isConnected[0].size();
+        DisjointSet ds(m);
+        for(int i = 0; i<m ; i++){
+            for(int j = i+1; j<n; j++){
+                if(isConnected[i][j] == 1){
+                    ds.unionbysize(i,j);
+                }
             }
         }
-        return count;
+
+        int cnt = 0;
+        for(int i = 0; i<m; i++){
+            if(ds.findUpar(i) == i){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
