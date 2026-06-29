6acf7021540c2322db6d80945631a6b79af0682a
Vaishnavi Rai <144893427+VaishnaviRai287@users.noreply.github.com>
2026-06-29 17:09:08 +0530

    Time: 2 ms (18.84%) | Memory: 9.1 MB (55.51%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
 
-    int helper(string &s, int i, long long num, int sign) {
-        if (i >= s.size() || !isdigit(s[i]))
+    int helper(string& s, int i, long long num, int sign){
+        if(i>= s.size() || !isdigit(s[i])){
             return sign * num;
-        int digit = s[i] - '0';
-        if (num > INT_MAX / 10 ||
-           (num == INT_MAX / 10 &&
-            digit > (sign == 1 ? 7 : 8))) {
-            return sign == 1 ? INT_MAX : INT_MIN;
         }
-        return helper(s, i + 1,num * 10 + digit,sign);
+        int digit = s[i] - '0';
+        if(num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > (sign == 1 ? 7 : 8))){
+            return sign == 1? INT_MAX : INT_MIN;
+        }
+        return helper(s, i+1, num*10 + digit, sign);
     }
     int myAtoi(string s) {
         int i = 0;
-        while (i < s.size() && s[i] == ' ')
-            i++;
+        while(i<s.size() && s[i] == ' ') i++;
         int sign = 1;
-        if (i < s.size() &&
-           (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-')
-                sign = -1;
+        if(i<s.size() && (s[i] == '+' || s[i] == '-')){
+            if(s[i] == '-') sign = -1;
             i++;
         }
         return helper(s, i, 0, sign);
