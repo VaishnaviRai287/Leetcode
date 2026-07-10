6025ef0f69065af3c8eced3d88120e89d70779d4
Vaishnavi Rai <144893427+VaishnaviRai287@users.noreply.github.com>
2026-07-10 15:50:04 +0530

    Time: 84 ms (70.97%) | Memory: 239.7 MB (31.75%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    bool canCross(vector<int>& stones) {
-        int n = stones.size();
-        if (stones[1] != 1)
-            return false;
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++)
-            mp[stones[i]] = i;
-        vector<vector<bool>> dp(n, vector<bool>(n + 1, false));
-        dp[1][1] = true;
-        for(int i = 1; i < n; i++){
-            for(int jump = 1; jump <= n; jump++){
-                if(!dp[i][jump])
-                    continue;
-                for(int nextJump = jump - 1;
-                    nextJump <= jump + 1;
-                    nextJump++){
-                    if(nextJump <= 0)
-                        continue;
-                    int nextStone = stones[i] + nextJump;
-                    if(mp.count(nextStone))
-                        dp[mp[nextStone]][nextJump] = true;
+    unordered_map<int,int> mp;
+    vector<vector<int>> dp;
+    bool solve(int idx, int jump, vector<int>& stones){
+        if(idx == stones.size()-1) return true;
+        if(dp[idx][jump] != -1) return dp[idx][jump];
+        for(int nextJump = jump-1; nextJump<= jump+1; nextJump++){
+            if(nextJump<=0) continue;
+            int nextStone = stones[idx] + nextJump;
+            if(mp.find(nextStone)!= mp.end()){
+                int nextIndex = mp[nextStone];
+                if(solve(nextIndex,nextJump, stones)){
+                    return dp[idx][jump] = true;
                 }
             }
         }
-        for(int k = 1; k <= n; k++)
-            if(dp[n-1][k])
-                return true;
-        return false;
-
+        return dp[idx][jump] = false;
+    }
+    bool canCross(vector<int>& stones) {
+        if(stones[1] != 1) return false;
+        int n = stones.size();
+        dp.assign(n, vector<int>(n+1,-1));
+        for(int i = 0; i< stones.size(); i++){
+            mp[stones[i]] = i;
+        }
+        return solve(1,1,stones);
     }
 };
