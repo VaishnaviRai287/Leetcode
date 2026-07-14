aebb0ce7efdfacb66d5cd8d3721f28cb17892e2c
Vaishnavi Rai <144893427+VaishnaviRai287@users.noreply.github.com>
2026-07-14 12:40:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (85.63%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -3,17 +3,18 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, obstacleGrid, dp);
-    }
-    int f(int i, int j, vector<vector<int>>& mat, vector<vector<int>>& dp){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(i>=0 && j>=0 && mat[i][j] == 1) return dp[i][j] = 0;
-        if(i==0 && j==0) return dp[i][j] = 1;
-
-        int up = f(i-1, j, mat, dp);
-        int left = f(i, j-1, mat, dp);
-        return dp[i][j] = up+left;
+        if (obstacleGrid[0][0] == 1) return 0;
+        vector<int> dp(n, 0); 
+        dp[0] = 1; 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++){
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0; 
+                } else if (j > 0) {
+                    dp[j] = dp[j] + dp[j-1]; 
+                } 
+            }
+        }
+        return dp[n-1];
     }
 };
